80fa8ca9717da1b9040df2d30b33888e766dd19a
davide <davide.dantonio@webeetle.com>
2018-12-12 09:09:55 +0100

    properties and functions

---
+++ 01-playing-with-syntax/18_defining_properties.js
@@ -0,0 +1,4 @@
+let x = 1, y = 2;
+
+let object = { x, y };
+console.log(object.x); //output =”1”
---
+++ 01-playing-with-syntax/19_defining_object_functions.js
@@ -0,0 +1,7 @@
+let object = {
+  myFunction(){
+    console.log("Hello World!!!");
+  }
+}
+
+object.myFunction();
